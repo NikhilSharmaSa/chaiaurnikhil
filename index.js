7bd9f38dec0a090730582f4244a552e3280d25be
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2025-01-28 19:47:53 +0530

    index File

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("Hello Nikhil this is first Express File Production Ready  File!")
 })
+
+app.get("/dmeo",(req,res)=>{
+res.send("This is demo File")
+})
 app.listen(process.env.PORT,()=>{
     
     console.log(`port is listining at ${process.env.PORT}`);
